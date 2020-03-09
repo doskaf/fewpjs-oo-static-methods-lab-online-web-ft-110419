420751864b24e2bd32a31014d48859e978203a58
Dominique Skaf <doskaf@yahoo.com>
2020-03-09 00:44:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ class Formatter {
   
   static titleize(string) {
     let shouldBeLowercase = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
-    let stringArr = string.split(" ")
+    let stringArr = string.split(" ");
+    
   }
 }
