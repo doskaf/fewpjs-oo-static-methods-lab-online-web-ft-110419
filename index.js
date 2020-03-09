37fc6b1a4502d05c98624709ae7081362381c002
Dominique Skaf <doskaf@yahoo.com>
2020-03-09 00:46:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ class Formatter {
         finalStringArr.push(this.capitalize(element));
       }
     })
-    return finalStringArr.join(" ")
+    return this.capitalize(finalStringArr.join(" "))
   }
 }
